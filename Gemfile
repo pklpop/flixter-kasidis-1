67d596eaf957b98cbbeaba8087a56ce57b49811b
Kasidis <pklpop@hotmail.com>
2015-09-12 19:22:06 +0000

    payment setup with stripe gem

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ gem 'carrierwave-aws'
 
 gem 'mini_magick'
 
+gem 'stripe'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,20 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson, :only => [:show]
+
 	def show
 	end
 
 	private
+
+	def require_authorized_for_current_lesson
+		this_course = current_lesson.section.course
+
+		if current_user.enrolled_in?(this_course) != true
+			redirect_to course_path(this_course), :alert => 'Please enroll in this course!'
+		end
+	end
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
